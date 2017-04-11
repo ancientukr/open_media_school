e10da4c2024eba29818bccc5ed4642b850451334
fedor <menshakov2017@gmail.com>
2017-04-12 02:47:34 +0300

    main page

--- open_media_school/settings.py
+++ open_media_school/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'azpnnr9gl96ds+!6qnzq3)(-hkacutb!=h0oi)3ja)zpbrvxm2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mediaschool.pythonanywhere.com', 'http://127.0.0.1:8000']
 
 
 # Application definition
